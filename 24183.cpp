7cee691058c3f578ea164659462f14b47faef034
unknown <mjk54565@gmail.com>
2022-03-22 02:23:58 +0900

    .

--- 24183.cpp
+++ 24183.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	
+	double a, b, c;
+	cin >> a >> b >> c;
+
+	cout << 2 * (a / 10000) * (22.9 * 32.4) + 2 * (b / 10000) * (29.7 * 42.0) + (c / 10000) * (21.0 * 29.7);
 }
--- 5430.cpp
+++ 5430.cpp
@@ -1,11 +1,72 @@
 #include <iostream>
-#include <vector>
-#include <tuple>
+#include <string>
+#include <queue>
 using namespace std;
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
+	int t;
+	cin >> t;
+	while (t--) {
+		string fun;
+		cin >> fun;
 
+		int n;
+		cin >> n;
 
+		string arr;
+		cin >> arr;
+
+		deque<int> dq;
+		string temp;
+
+		for (int i = 0; i < arr.length(); i++) {
+			if (arr[i] == '[') continue;
+			else if ('0' <= arr[i] && arr[i] <= '9') temp += arr[i];
+			else if(arr[i] == ','||arr[i]==']')
+				if (!temp.empty()) {
+					dq.push_back(stoi(temp));
+					temp.clear();
+				}
+		}
+
+		bool flag = true;
+		bool left = true;
+		for (int i = 0; i < fun.length(); i++) {
+			if (fun[i] == 'R') left = !left;
+			else {
+				if (dq.empty()) {
+					flag = false;
+					cout << "error\n";
+					break;
+				}
+				else {
+					if (left) dq.pop_front();
+					else dq.pop_back();
+				}
+			}
+		}
+
+		if (flag) {
+			if (left) {
+				cout << '[';
+				while (!dq.empty()) {
+					cout << dq.front();
+					dq.pop_front();
+					if (!dq.empty()) cout << ',';
+				}
+				cout << "]\n";
+			}
+			else {
+				cout << '[';
+				while (!dq.empty()) {
+					cout << dq.back();
+					dq.pop_back();
+					if (!dq.empty()) cout << ',';
+				}
+				cout << "]\n";
+			}
+		}
+	}
 }
