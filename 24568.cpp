597b60e4022dcd78c6a3344c33f5905ce29b8c6b
minjun <mjk54565@gmail.com>
2022-05-22 23:18:22 +0900

    .

---
+++ 24568.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main() {
+	cin.tie(0);
+	std::ios_base::sync_with_stdio(0);
+
+	int a, b;
+	cin >> a >> b;
+	cout << a * 8 + b * 3 - 28;
+}
