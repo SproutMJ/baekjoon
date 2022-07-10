57134e39cd0e2b2958b3a272c74dffa41b8073d8
minjun <mjk54565@gmail.com>
2022-07-10 15:41:29 +0900

    .

---
+++ 25314.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main() {
+	cin.tie(0);
+	std::ios_base::sync_with_stdio(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n / 4; i++)cout << "long ";
+	cout << "int";
+}
