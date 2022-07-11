447f144e898e568a61f97e93fb6904374b06fd09
minjun <mjk54565@gmail.com>
2022-07-11 20:48:19 +0900

    .

--- 3733.cpp
+++ 3733.cpp
@@ -4,8 +4,12 @@ int main() {
 	cin.tie(0);
 	std::ios_base::sync_with_stdio(0);
 
-	int n;
-	cin >> n;
-	for (int i = 0; i < n / 4; i++)cout << "long ";
-	cout << "int";
+	while (1) {
+		int a, b;
+		if (cin >> a >> b) {
+			cout << b / (a + 1) << "\n";
+		}
+		else
+			break;
+	}
 }
