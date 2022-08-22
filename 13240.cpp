65d1a26ee251946b14a0a17f9b08d7ac25759269
SproutMJ <mjk54565@gmail.com>
2022-08-22 22:14:58 +0900

    .

---
+++ 13240.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    std::ios_base::sync_with_stdio(0);
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++)
+            if ((i + j) % 2 == 0)cout << '*';
+            else cout << '.';
+        cout << "\n";
+    }
+}
