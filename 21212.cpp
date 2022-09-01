09526266599f5fa920f572ec6c581606bc2b83eb
minjun <mjk54565@gmail.com>
2022-09-01 10:07:59 +0900

    .

---
+++ 21212.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+int main() {
+	cin.tie(0);
+	std::ios_base::sync_with_stdio(0);
+
+	int n, ans = 987654321;
+	cin >> n;
+	while (n--) {
+		int a, b;
+		cin >> a >> b;
+		ans = min(ans, b / a);
+	}
+
+	cout << ans;
+}
