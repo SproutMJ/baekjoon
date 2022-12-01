20b013789c20eac6be6bf45eabf97f61a37421a0
minjun <mjk54565@gmail.com>
2022-12-01 23:34:06 +0900

    .

---
+++ 3765.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main() {
+	cin.tie(0);
+	std::ios_base::sync_with_stdio(0);
+
+	string s;
+	while (getline(cin, s)) {
+		cout << s << "\n";
+	}
+}
