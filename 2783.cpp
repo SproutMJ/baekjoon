1b55604c8a64091e75ba0be109c4cdf9b0fed5b5
minjun <mjk54565@gmail.com>
2022-12-06 21:22:37 +0900

    .

---
+++ 2783.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+int main() {
+	double a, b;
+	cin >> a >> b;
+	double min = a / b;
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a >> b;
+		min = min > (a / b) ? (a / b) : min;
+	}
+
+	cout << min * 1000 << "\n";
+}
