ed50486ccccaf6e9d99ba8d0299be34d0906e814
SproutMJ <mjk54565@gmail.com>
2022-12-12 20:58:09 +0900

    .

---
+++ 4740.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main(){
+    cin.tie(0);
+    std::ios_base::sync_with_stdio(0);
+
+    while(1){
+        string s;
+        getline(cin, s);
+        if(s == "***")
+            break;
+
+        reverse(s.begin(), s.end());
+        cout << s<<"\n";
+    }
+}
