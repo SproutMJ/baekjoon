f4866064aabfadb0fd489ef56d781364276a23bb
SproutMj <mjk54565@gmail.com>
2024-03-16 19:47:34 +0900

    .

---
+++ 31606.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    std::ios_base::sync_with_stdio(0);
+
+    int x, y;
+    cin >> x >> y;
+    cout << x +y+3;
+}
